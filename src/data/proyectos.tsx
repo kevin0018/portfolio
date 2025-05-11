8c769ed30cd0d4d9116319a1f7bb7f4b779d487b
Kevin Hernández <akevin.2215@gmail.com>
2025-05-12 00:33:14 +0200

    add MySQL and jQuery skills to habilidades, update project details in proyectos

--- src/data/proyectos.tsx
+++ src/data/proyectos.tsx
@@ -8,18 +8,18 @@ const proyectos = [
     },
     {
         nombre: "LiveChat GPT",
-        imagen: `${import.meta.env.BASE_URL}assets/images/livechat_gpt.png`,
+        imagen: `${import.meta.env.BASE_URL}assets/images/blog_de_viajes.png`,
         lenguajes: ["Docker", "MongoDB", "Node.js", "CSS"],
-        enlaceWeb: "",
-        enlaceGithub: ""
+        enlaceWeb: "https://kevin0018.github.io/Blog-de-Viajes/",
+        enlaceGithub: "https://github.com/kevin0018/Blog-de-Viajes"
 
     },
     {
         nombre: "Netflix 'Clone'",
-        imagen: `${import.meta.env.BASE_URL}assets/images/portfolio.png`,
-        lenguajes: ["React", "Tailwind CSS"],
-        enlaceWeb: "",
-        enlaceGithub: ""
+        imagen: `${import.meta.env.BASE_URL}assets/images/blog_de_viajes.png`,
+        lenguajes: ["Laravel", "MySQL", "CSS", "PHP", "JQuery"],
+        enlaceWeb: "https://kevin0018.github.io/Blog-de-Viajes/",
+        enlaceGithub: "https://github.com/kevin0018/Blog-de-Viajes"
     }
 
 ];
